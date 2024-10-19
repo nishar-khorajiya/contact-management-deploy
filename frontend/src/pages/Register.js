77d4c059159b94edac3d1692cf9804726cdaa959
Nishar Khorajiya <110724795+nishar-khorajiya@users.noreply.github.com>
2024-10-20 04:18:28 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/api/users/register', {
+      const res = await axios.post('contact-management-deploy-apis.vercel.app/api/users/register', {
         username,
         email,
         password,
